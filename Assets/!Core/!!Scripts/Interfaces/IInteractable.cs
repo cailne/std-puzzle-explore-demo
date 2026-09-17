e43f67e065ab43a95f7ac57e15ba1e06c06cf3e9
Leonardo-STCD-MacPro <leonardowijaya10@gmail.com>
2026-09-17 18:46:16 +0700

    interactable

---
+++ Assets/!Core/!!Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,7 @@
+namespace Lucielle
+{
+	public interface IInteractable
+	{
+		void Interact();
+	}
+}
--- Assets/!Core/!!Scripts/Player/PlayerInteraction.cs
+++ Assets/!Core/!!Scripts/Player/PlayerInteraction.cs
@@ -1,22 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Lucielle
 {
     public class PlayerInteraction : MonoBehaviour
     {
-        private GameObject objectSelected;
+        [SerializeField] private VoidEventChannelSO interactEventChannelSO;
+        private IInteractable interactable;
+
+        private void OnEnable()
+        {
+            interactEventChannelSO?.RegisterListener(CallInteract);
+        }
+
+        private void OnDisable()
+        {
+            interactEventChannelSO?.RemoveListener(CallInteract);
+        }
 
         private void OnTriggerEnter(Collider other)
         {
+            if (interactable != null) return;
 
+            IInteractable obj = other.GetComponent<IInteractable>();
+            if (obj != null)
+                interactable = obj;
         }
 
         private void OnTriggerExit(Collider other)
         {
+            interactable = null;
+        }
 
+        public void CallInteract()
+        {
+            if (interactable == null) return;
+            interactable.Interact();
         }
     }
 }
