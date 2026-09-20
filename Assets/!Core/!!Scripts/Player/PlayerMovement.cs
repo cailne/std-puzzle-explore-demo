2d2925340e3a366938837f1c1faac804edf870a5
SPEC-06/2025 <leonardowijaya10@gmail.com>
2026-09-20 21:01:56 +0700

    adding some free assets

--- Assets/!Core/!!Scripts/Player/PlayerMovement.cs
+++ Assets/!Core/!!Scripts/Player/PlayerMovement.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 
 namespace Lucielle
 {
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Modules")]
+        [SerializeField] private CharacterController controller;
+        [SerializeField] private Transform camTransform;
+        [Header("Properties")]
         [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private Transform lookTarget;
+        [SerializeField] private float turnSmoothTime = 0.1f;
+        [SerializeField] private bool isActive = true;
+        [Header("Channels")]
+        [SerializeField] private BoolEventChannelSO controlActivationEventChannelSO;
+
+        private float turnSmoothVelocity;
 
         private void Update()
         {
-            if(lookTarget != null)
-                transform.LookAt(lookTarget);
+            if (!isActive) return;
+
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * (moveSpeed * Time.deltaTime));
+            }
         }
 
         public void MoveUpWard()
--- Assets/!Core/!!Scripts/ScriptableObjects/Channels/BoolEventChannelSO.cs
+++ Assets/!Core/!!Scripts/ScriptableObjects/Channels/BoolEventChannelSO.cs
@@ -1,7 +1,9 @@
 using System;
+using UnityEngine;
 
 namespace Lucielle
 {
+    [CreateAssetMenu(menuName = "Scriptable Objects/Events/Bool Event Channel")]
 	public class BoolEventChannelSO : BaseEventChannelSO<bool>
 	{
 		public override void RegisterListener(Action<bool> action)
